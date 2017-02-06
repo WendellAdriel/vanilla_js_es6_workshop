9f79a46bd31924140609d177b7be5f876c20f6eb
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-02-06 22:23:26 +0000

    finished application

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+(function () {
+  const formSection    = document.getElementById('form');
+  const loadingSection = document.getElementById('loading');
+  const resultSection  = document.getElementById('result');
+  const searchSection  = document.getElementById('search-again');
+
+  const nameInput         = document.getElementById('name');
+  const searchButton      = document.getElementById('search-btn');
+  const searchAgainButton = document.getElementById('search-again-btn');
+  const pokemonImage      = document.getElementById('pokemon-img');
+  const pokemonName       = document.getElementById('pokemon-name');
+  const pokemonNumber     = document.getElementById('pokemon-number');
+  const pokemonTypes      = document.getElementById('pokemon-types');
+
+  const nameNotification    = document.getElementById('name-notification');
+  const requestNotification = document.getElementById('req-notification');
+  const requestErrorMessage = document.getElementById('req-error-msg');
+
+  const API_PATH = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const init = () => {
+    loadingSection.style.display      = 'none';
+    resultSection.style.display       = 'none';
+    searchSection.style.display       = 'none';
+    nameNotification.style.display    = 'none';
+    requestNotification.style.display = 'none';
+    formSection.style.display         = 'block';
+    nameInput.value                   = '';
+  };
+
+  const searchPokemon = () => {
+    const pokemonSearchName = nameInput.value.trim();
+
+    if (!isValidName(pokemonSearchName)) {
+      nameNotification.style.display = 'block';
+      nameInput.className           += ' is-danger';
+      return;
+    }
+
+    nameNotification.style.display = 'none';
+    nameInput.className            = 'input is-expanded';
+    makeRequest(pokemonSearchName);
+  };
+
+  const isValidName = pokemonSearchName => {
+    if (pokemonSearchName.length === 0) {
+      return false;
+    }
+    return true;
+  };
+
+  const makeRequest = pokemonSearchName => {
+    const url = `${API_PATH}${pokemonSearchName}`;
+    showLoading();
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => showResult(res))
+      .catch(error => showError(error));
+  };
+
+  const showLoading = () => {
+    formSection.style.display    = 'none';
+    loadingSection.style.display = 'block';
+  };
+
+  const showResult = response => {
+    if (response.detail) {
+      showError(`Pokemon ${response.detail}`);
+      return;
+    }
+    pokemonName.innerHTML   = response.name.toUpperCase();
+    pokemonNumber.innerHTML = `#${response.id}`;
+    pokemonTypes.innerHTML  = buildTypesTags(response.types);
+    pokemonImage.src        = response.sprites.front_default;
+
+    loadingSection.style.display = 'none';
+    resultSection.style.display  = 'block';
+    searchSection.style.display  = 'block';
+  };
+
+  const buildTypesTags = types => {
+    let typesTags = '';
+
+    types.forEach(type => {
+      const typeName    = type.type.name.toUpperCase();
+      const tagTemplate = `<span class="tag">${typeName}</span>`;
+      typesTags        += tagTemplate;
+    });
+
+    return typesTags;
+  };
+
+  const showError = error => {
+    requestErrorMessage.innerHTML     = error;
+    loadingSection.style.display      = 'none';
+    requestNotification.style.display = 'block';
+    searchSection.style.display       = 'block';
+  };
+
+  init();
+  searchButton.addEventListener('click', searchPokemon);
+  searchAgainButton.addEventListener('click', init);
+})();
